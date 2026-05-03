edabf1b353b0288dc00208f676dba8d7e66f4e6d
Vansh2003 <Vanshmistry08@gmail.com>
2026-05-03 14:34:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function showBiodata(){
+document.getElementById("name").innerHTML="Smit Mistry";
+document.getElementById("address").innerHTML="Ahmedabad, Gujarat";
+document.getElementById("pincode").innerHTML="380001";
+document.getElementById("state").innerHTML="Gujarat";
+document.getElementById("city").innerHTML="Ahmedabad";
+document.getElementById("age").innerHTML="20";
+document.getElementById("dob").innerHTML="01/01/2005";
+}
+
+function changePic(){
+document.getElementById("profilePic").src="images.jpg";
+}
+
+function changecolor(){
+  alert("Button Working");
+  document.body.style.backgroundColor="red";
+}
